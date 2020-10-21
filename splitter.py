85ee518a1f765e9d937661000155741b1ef7aadc
Ivy <ivyjs@hendrix.edu>
2020-10-20 20:33:33 -0500

    Added reading from command line

--- splitter.py
+++ splitter.py
@@ -1,36 +1,40 @@
 import os
-
-training_dir = "/Users/ivy/Desktop/Senior_Seminar/training"
-testing_dir = "/Users/ivy/Desktop/Senior_Seminar/testing"
-split_factor = 10
+import sys
 
 def listdirs(path):
     return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 
-for dir in listdirs(training_dir):
-    current_dir = f"{training_dir}/{dir}"
-    current_testing_dir = f"{testing_dir}/{dir}"
-    all_files = os.listdir(current_dir)
+if len(sys.argv)>=3:
 
-    num_files = len(all_files)
-    #Get 10% of all files    
-    num_test = round(num_files/split_factor)
+    training_dir = sys.argv[1]
+    testing_dir = sys.argv[2]
+    if len(sys.argv)==4:
+        split_factor = sys.argv[3]
+    else:
+       split_factor = 10
 
-    training_files = all_files[num_test:num_files]
-    test_files =  all_files[0:num_test]
+    for dir in listdirs(training_dir):
+        current_dir = f"{training_dir}/{dir}"
+        current_testing_dir = f"{testing_dir}/{dir}"
+        all_files = os.listdir(current_dir)
 
-    test_files_dir_before_moves = list(map((lambda n:  f"{current_dir}/{n}"), test_files))
-    test_files_dir_after_moves = list(map((lambda n: f"{current_testing_dir}/{n}"), test_files))
+        num_files = len(all_files)
+        #Get 10% of all files    
+        num_test = round(num_files/split_factor)
 
-    if not os.path.exists(current_testing_dir):
-        os.makedirs(current_testing_dir)    
+        training_files = all_files[num_test:num_files]
+        test_files =  all_files[0:num_test]
 
-    for i in range(len(test_files_dir_before_moves)):
-        before_move = test_files_dir_before_moves[i]
-        after_move = test_files_dir_after_moves[i]
-        print(f"Moving {before_move} {after_move}")
-        os.rename(before_move, after_move)
+        test_files_dir_before_moves = list(map((lambda n:  f"{current_dir}/{n}"), test_files))
+        test_files_dir_after_moves = list(map((lambda n: f"{current_testing_dir}/{n}"), test_files))
 
-    # map(lambda before_move, after_move: os.rename(before_move, after_move), test_files_dir_before_moves, test_files_dir_after_moves)
+        if not os.path.exists(current_testing_dir):
+            os.makedirs(current_testing_dir)    
 
-    
+        for i in range(len(test_files_dir_before_moves)):
+            before_move = test_files_dir_before_moves[i]
+            after_move = test_files_dir_after_moves[i]
+            print(f"Moving {before_move} {after_move}")
+            os.rename(before_move, after_move)
+else:
+    print("Usage: python3 splitter.py training_dir testing_dir [split_factor] ")
